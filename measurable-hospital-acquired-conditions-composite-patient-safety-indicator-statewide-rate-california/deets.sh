c78a2625ab027c642ec6cf3369667aed8fa3046c
Ray Kiddy <ray@ganymede.org>
2024-08-08 14:23:06 -0700

    First

---
+++ measurable-hospital-acquired-conditions-composite-patient-safety-indicator-statewide-rate-california/deets.sh
@@ -0,0 +1,6 @@
+# The ID may end in "california". This is a weird one.
+export uuid1="1fdde521-4646-4220-8084-f6b5d57b67a9"
+export uuid2="52976bca-0b9e-4904-8034-28d2fd06d8e1"
+export id="measurable-hospital-acquired-conditions-composite-patient-safety-indicator-statewide-rate-califo"
+export hash=""
+export script="none" 
