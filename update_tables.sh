2ef95a4f8dab0263b333201ee62db0620a5568bb
Ray Kiddy <ray@ganymede.org>
2024-09-01 21:05:39 -0700

    Main page contains all datasets, not just fetched datasets.

--- update_tables.sh
+++ update_tables.sh
@@ -31,28 +31,22 @@ echo ""
 
 mysqldump --no-data ca_hhs | grep -v '\/\*' > tables.sql
 
-echo "Regenerating the ca_hhs_www/templates/main.html file"
-echo ""
-
 # make sure there is a top file for every dataset, even if it is empty.
 #
 d="ca_hhs_www/templates/tops"
-
 cat tables.txt | grep dataset | \
     awk '{print "if [ ! -f '$d'/"$2"-top.html ]; then touch '$d'/"$2"-top.html ; fi"}' | \
     bash
 
-cat <<EOF > /tmp/ca_hhs_$$.html
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
-<html>
-<head>
-    <title>HCAI</title>
-    {% include "styles.html" %}
-</head>
-<body>
-<div style="padding: 25px 25px 25px 25px;">
-<h2>Datasets:</h2>
-EOF
+echo "Are there duplicate directories in unused?"
+
+find * -type d -prune > /tmp/ca_hhs_$$_1.txt
+( cd unused ; find * -type d -prune >> /tmp/ca_hhs_$$_1.txt )
+cat /tmp/ca_hhs_$$_1.txt | sort | uniq -c | awk '{if ($1 != 1) print $0}'
+
+echo ""
+echo "Regenerating the ca_hhs_www/templates/main.html file"
+echo ""
 
 cat tables.txt | \
     awk '{if ($1 == "dataset") print ""; print $2}' | \
@@ -73,13 +67,31 @@ cat /tmp/ca_hhs_work_$$.txt | \
 
 cat /tmp/ca_hhs_work2_$$.txt | \
     awk 'BEGIN{FS="\n";RS=""}
-         {print "<p>"$1"<br/>tables # "$2", updated: "$3"<br/>{% include \"tops/"$1"-top.html\" %}</p>"}' >> /tmp/ca_hhs_$$.html
+         {print "<p>"$1"<br/>tables # "$2", updated: "$3"<br/>{% include \"tops/"$1"-top.html\" %}</p>"}' >> /tmp/ca_hhs_work3_$$.html
 
-cat <<EOF >> /tmp/ca_hhs_$$.html
+find unused/* -type d -prune | \
+    grep -v deleted | \
+    awk 'BEGIN{FS="/"}{print "<p>"$2"<br/>"}' >> /tmp/ca_hhs_work3_$$.html
+
+cat <<EOF > /tmp/ca_hhs_$$_2.html
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<html>
+<head>
+    <title>HCAI</title>
+    {% include "styles.html" %}
+</head>
+<body>
+<div style="padding: 25px 25px 25px 25px;">
+<h2>Datasets:</h2>
+EOF
+
+sort < /tmp/ca_hhs_work3_$$.html >> /tmp/ca_hhs_$$_2.html
+
+cat <<EOF >> /tmp/ca_hhs_$$_2.html
 <p>or see <a href="/hcai/table_columns/">table columns</a>.
 </div>
 </body></html>
 EOF
 
-mv /tmp/ca_hhs_$$.html ca_hhs_www/templates/main.html
+mv /tmp/ca_hhs_$$_2.html ca_hhs_www/templates/main.html
 
