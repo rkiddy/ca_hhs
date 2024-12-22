ba8847dfbe29b4ab853ba3ee7e0a6ddeb4a58e4a
Ray Kiddy <ray@ganymede.org>
2024-12-22 14:39:53 -0800

    Extremely helpful methods for setting up transfer of csv files to the database.

--- csv_helper.py
+++ csv_helper.py
@@ -13,6 +13,7 @@ conn = engine.connect()
 
 
 def db_exec(eng, this_sql):
+    """Helpful method, could be put into a module or something easier to manage."""
     # print(f"sql: {sql}")
     if this_sql.strip().startswith('select'):
         return [dict(row) for row in eng.execute(this_sql).fetchall()]
@@ -22,10 +23,38 @@ def db_exec(eng, this_sql):
 
 
 def db_exec_sql(sql):
+    """Assume the existing connection, because this is what happens anway."""
     return db_exec(conn, sql)
 
 
+def db_exec_many(conn, prefix, suffixes):
+    """Execute a large bunch of sql statements but if there is an error,
+       fall back to doing one at a time. This makes large sets of insertions massively faster."""
+
+    done = False
+    try:
+        sql = f"{prefix} {',\n'.join(suffixes)}"
+        db_exec(conn, sql)
+        done = True
+    except:
+        print("EXCEPTION:")
+
+    if not done:
+        try:
+            for suffix in suffixes:
+                sql = f"{prefix} {suffix}"
+                db_exec(conn, sql)
+        except:
+            print("EXCEPTION:")
+            traceback.print_exc()
+
+
+def db_exec_many_sql(prefix, suffixes):
+    return db_exec_many(conn, prefix, suffixes)
+
+
 def fix(start):
+    """For putting a string into a query or insert SQL statement."""
     if not start:
         return 'NULL'
     else:
@@ -34,6 +63,7 @@ def fix(start):
 
 
 def fix_int(start):
+    """For putting an integer or other number into a query or insert SQL statement."""
     if not start:
         return 'NULL'
     else:
@@ -41,6 +71,9 @@ def fix_int(start):
 
 
 def fix_date(start):
+    """Dates often occur in other forms, like MM-DD-YYYY, and this turns these into a
+       proper mysql date string."""
+
     if start is None or start == '':
         return 'NULL';
     parts = start.split(' ')[0].split('/')
@@ -61,10 +94,52 @@ def fix_date(start):
     return f"'{year}-{mon}-{dat}'"
 
 
+known_replacements = {'system': 'system_name',
+                      'rank': 'rank_name',
+                      'procedure': 'procedure_name'}
+
 def fix_col_head(start, replaces={}):
-    col = fix_first_key(start.lower().replace(' ', '_').replace('-', '_').replace('/', '_').replace('+_', '_plus_'))
+    """Many of the column headers in these files are not compatible with mysql databases."""
+
+    col = fix_first_key(start)
+
     for key in replaces:
         col = col.replace(key, replaces[key])
+
+    col = col.lower()
+
+    for key in known_replacements:
+        col = col.replace(key, known_replacements[key])
+
+    while col.startswith(' '):
+        col = col[1:]
+    while col.endswith(' '):
+        col = col[:-1]
+    col = col.replace(' ', '_')
+    col = col.replace('-', '_')
+    col = col.replace('(', '')
+    col = col.replace(')', '')
+    col = col.replace('/', '_')
+    col = col.replace(',', '_')
+    col = col.replace('.', '_')
+    col = col.replace('+_', '_plus_')
+    col = col.replace('#', 'num')
+    col = col.replace('%', 'pct')
+    col = col.replace('?', '')
+    col = col.replace('>=', '_ge_')
+    col = col.replace('<=', '_le_')
+    col = col.replace('"', '')
+    col = col.replace(':', '')
+    col = col.replace('>', '_gt_')
+    col = col.replace('<', '_lt_')
+    col = col.replace('=', '_eq_')
+    col = col.replace('^', '')
+
+    col = col.replace('__', '_')
+
+    for key in replaces:
+        col = col.replace(key, replaces[key])
+
     return col
 
 
@@ -74,6 +149,8 @@ def fix_col_heads(start, replaces={}):
 
 
 def fix_first_key(key):
+    """At times, just the first column header in a spreadsheet has an anomolous character at its start."""
+
     first = list(key)
     offset = 0
     for idx in range(len(first)):
@@ -86,11 +163,17 @@ def fix_first_key(key):
         return key[offset+1:]
 
 
-def create_tables(tables, types={}, replaces={}):
+def create_tables(tables, types={}, replaces={}, length_pad=0, verbose=False):
+    """Do the right thing for most csv files based on the column header strings found.
+       Also determines the optimal length for a string column, though this process reads
+       all of the data and so is probably not optimal and is probably doable in a smarter
+       way."""
 
     for table in tables:
 
         file = tables[table]
+        if verbose:
+            print(f"file: {file}")
 
         cols = list()
         lengths = dict()
@@ -101,28 +184,46 @@ def create_tables(tables, types={}, replaces={}):
             # read through the file once to create the table. Wasteful? Maybe, but more sure.
             #
             for row in rdr:
+                if '' in row:
+                    row.pop('')
+                if None in row:
+                    row.pop(None)
 
                 next_row = dict()
                 for key in row:
                     next_row[fix_col_head(key, replaces)] = row[key]
                 row = next_row
+                if verbose:
+                    print(f"row: {row}")
 
                 if len(cols) == 0:
 
                     cols = list(row.keys())
                     for col in cols:
                         lengths[col] = 0
+                    if verbose:
+                        print(f"starting columns: {col}")
 
                 for key in row:
-                    if len(row[key]) > lengths[key]:
-                        lengths[key] = len(row[key])
+                    try:
+                        if len(row[key]) > lengths[key]:
+                            lengths[key] = len(row[key])
+                    except:
+                        if verbose:
+                            print(f"bad row: {row}")
+
+        # print(f"types: {types}\n")
 
         col_defs = list()
+        # print(f"lengths: {lengths}")
         for col in lengths:
+            # print(f"col: {col}")
             if table in types and col in types[table]:
                 col_defs.append(f"{col} {types[table][col]}")
+                # print("MATCH")
             else:
-                col_defs.append(f"{col} varchar({lengths[col]})")
+                col_defs.append(f"{col} varchar({lengths[col] + length_pad})")
+                # print("NO match")
 
         sql = f"create table {table} ("
         sql += ', '.join(col_defs)
@@ -136,7 +237,8 @@ def create_tables(tables, types={}, replaces={}):
         db_exec(conn, sql)
 
 
-def read_data(tables, types={}, replaces={}):
+def read_data(tables, types={}, replaces={}, start_row=None, bucket=1000):
+    """Reads CSV data and puts it into a table."""
 
     # print(f"read_data: types: {types}")
 
@@ -150,19 +252,32 @@ def read_data(tables, types={}, replaces={}):
         # print(f"files: {files}")
 
         cols = None
+        prefix = None
 
         num = 0
 
         for f in files:
+
+            print(f"\nfile: {f} -> ", end='')
+
+            sqls = list()
 
             with open(f, newline='', encoding='latin1') as csvfile:
                 rdr = csv.DictReader(csvfile)
 
                 for row in rdr:
 
+                    if '' in row:
+                        row.pop('')
+                    if None in row:
+                        row.pop(None)
+
                     if cols is None:
                         cols = fix_col_heads(list(row.keys()), replaces)
                         # print(f"read_data: fixed cols: {cols}")
+
+                    if start_row is not None and rdr.line_num < start_row:
+                        continue
 
                     next_vals = list()
 
@@ -185,12 +300,27 @@ def read_data(tables, types={}, replaces={}):
 
                         next_vals.append(val)
 
-                    sql = f"insert into {table} ({', '.join(cols)}) values ({', '.join(next_vals)})"
+                    if not prefix:
+                        prefix = f"insert into {table} ({', '.join(cols)}) values"
+
+                    sqls.append(f"({', '.join(next_vals)})")
+
+                    if len(sqls) >= bucket:
+                        db_exec_many(conn, prefix, sqls)
+                        sqls.clear()
+
+                    # When we were doing this one at a time.
+                    #
+                    # sql = f"insert into {table} ({', '.join(cols)}) values ({', '.join(next_vals)})"
                     # print(f"sql: {sql}")
-                    db_exec(conn, sql)
+                    # db_exec(conn, sql)
+
                     num += 1
 
-            print(f"\nfile: {f} -> table: {table} # {num}")
+            if len(sqls) > 0:
+                db_exec_many(conn, prefix, sqls)
+
+            print(f"table: {table} # {num}")
 
 
 if __name__ == '__main__':
