f3b94526e3f6a89b2a841bd348cdebeeccf3a6ff
Ray Kiddy <ray@ganymede.org>
2024-12-22 14:29:22 -0800

    Becoming a bit of a mess, trying to handle lots of special conditions.

--- update_one.sh
+++ update_one.sh
@@ -12,7 +12,7 @@ if [ "$1" = "--help" ] || [ "$1" = "-help" ] || [ "$1" = "-h" ] || [ "$1" = "--h
     echo ""
     echo "usage: bash update.sh [ --no-fetch | --only-fetch ]"
     echo ""
-    exit 
+    exit
 fi
 
 if [ "$1" != "" ] && [ "$1" != "--no-fetch" ] && [ "$1" != "--only-fetch" ]; then
@@ -37,6 +37,10 @@ id=`pwd | awk 'BEGIN{FS="/"}{print $NF}'`
 
 source ./deets.sh
 
+( echo "select c1.file_name, c1.table_name from datasets d1, csv_sources c1";
+  echo " where d1.pk = c1.ds_pk and auto_run = 1 and d1.name = '$id';" ) | \
+    mysql --user=ray --password=alexna11 --skip-column-names ca_hhs_meta 2>/dev/null > /tmp/csv_$$.txt
+
 if [ "$1" != "--no-fetch" ] && [ "$uuid1" != "none" ]; then
 
     echo "fetching..."
@@ -59,17 +63,31 @@ if [ "$1" != "--no-fetch" ] && [ "$uuid1" != "none" ]; then
             {print "if [ ! -f \""$NF"\" ]; then wget '\''"$0"'\''; fi"}'
 
     else
+        err="no"
+
         if [ "$hash" != "" ]; then
+            echo "fetching: https://data.chhs.ca.gov/dataset/$uuid1/resource/$uuid2/download/$id-$hash.zip"
             wget -q "https://data.chhs.ca.gov/dataset/$uuid1/resource/$uuid2/download/$id-$hash.zip"
+            if [ $? -ne 0 ]; then
+                err="yes"
+            fi
         else
+            echo "fetching: https://data.chhs.ca.gov/dataset/$uuid1/resource/$uuid2/download/$id.zip"
             wget -q "https://data.chhs.ca.gov/dataset/$uuid1/resource/$uuid2/download/$id.zip"
+
+            if [ $? -ne 0 ]; then
+                echo "fetching: https://data.chhs.ca.gov/dataset/$uuid1/resource/$uuid2/download/$id-.zip"
+                wget -q "https://data.chhs.ca.gov/dataset/$uuid1/resource/$uuid2/download/$id-.zip"
+                if [ $? -ne 0 ]; then
+                    err="yes"
+                fi
+            fi
         fi
 
-        if [ $? -ne 0 ]; then
+        if [ $err = "yes" ]; then
             echo ""
-            echo "There was some error on downloading. Aborting script."
+            echo "There was some error on downloading."
             echo ""
-            exit 1
         fi
     fi
 
@@ -78,6 +96,33 @@ if [ "$1" != "--no-fetch" ] && [ "$uuid1" != "none" ]; then
     if [ -f fetch_extra.sh ]; then
         bash fetch_extra.sh
     fi
+fi
+
+if [ -f exec_special.sh ]; then
+
+    echo "executing special..."
+    bash exec_special.sh
+    if [ $? -ne 0 ]; then
+        echo "exec_special led to an ERROR."
+        echo ""
+        exit
+    fi
+    echo ""
+fi
+
+if [ ! -f $script.py ] && [ -s /tmp/csv_$$.txt ]; then
+    echo "yes. there are csv sources"
+
+    SQLALCHEMY_SILENCE_UBER_WARNING=1 ../.venv/bin/python3 ../csv_import.py --file /tmp/csv_$$.txt
+    r=$?
+
+    /bin/rm -f sources/*
+
+    mv -f *.csv *.xls* *.accdb *.docx *.html *.pdf *.pptx *.website \
+          *.web-link *.chart *.kml *.geojson *.json *-api *.zip \
+          sources/ 2>/dev/null
+
+    SQLALCHEMY_SILENCE_UBER_WARNING=1 ../.venv/bin/python ../update_time.py --result $r
 fi
 
 if [ "$1" != "--only-fetch" ] && [ -f $script.py ]; then
@@ -92,16 +137,12 @@ if [ "$1" != "--only-fetch" ] && [ -f $script.py ]; then
         r=$?
     fi
 
-    # get rid of any older copies of the data sources.
-    #
     /bin/rm -f sources/*
 
-    # move the current data sources to the sources directory
-    #
     mv -f *.csv *.xls* *.accdb *.docx *.html *.pdf *.pptx *.website \
           *.web-link *.chart *.kml *.geojson *.json *-api *.zip \
           sources/ 2>/dev/null
-fi
 
-SQLALCHEMY_SILENCE_UBER_WARNING=1 ../.venv/bin/python ../update_time.py
+    SQLALCHEMY_SILENCE_UBER_WARNING=1 ../.venv/bin/python ../update_time.py --result $r
+fi
 
