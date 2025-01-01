330f647de2cee4fd0ca732b4865ed23f4f2afbfb
Ray Kiddy <ray@ganymede.org>
2024-12-31 17:38:41 -0800

    extra fetching

---
+++ hospital-annual-financial-disclosure-report-complete-data-set/fetch_special_instead.sh
@@ -0,0 +1,41 @@
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/d94ddeea-30ba-47a1-90af-f8f5e10f4130/download/48hospitaldata.xlsx"
+n="48hospitaldata.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/29e972ce-84ae-4c54-9d62-937178cc61bc/download/47hospitaldata.xlsx"
+n="47hospitaldata.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/21be3412-9947-470f-a434-0c9165eba2b2/download/46hospitaldata.xlsx"
+n="46hospitaldata.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/b189d4d7-a57c-4138-93c1-d23629050282/download/45hospitaldata.xlsx"
+n="45hospitaldata.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/fab4c672-b6cc-4350-883d-577a2080b29d/download/44hospitaldata.xlsx"
+n="44hospitaldata.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/ae7c2985-a52b-438e-9588-3aeda33376ad/download/42hospitaldata.xlsx"
+n="42hospitaldata.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/e3acc50e-7dc7-4396-9076-85ad04a9bd8e/download/mr20151641hospitaldatafinutil.csv"
+n="mr20151641hospitaldatafinutil.csv"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/078f6884-a6f2-44db-87a7-d85dfec058be/download/39hospitaldata.xlsx"
+n="39hospitaldata.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/9c594ceb-fe46-4e6d-9553-c138a868bdc5/resource/e56c4d1d-9b3f-4906-83c7-17c33463dd0d/download/hadrexcel-full-db-pcl-labels-2004-2010.xlsx"
+n="hadrexcel-full-db-pcl-labels-2004-2010.xlsx"
+if [ ! -f $n ]; then wget $f; fi
+
+f="https://data.chhs.ca.gov/dataset/73f46925-ec89-4ba8-b2d4-c8e371a7776b/resource/5693046a-93ee-416a-afcd-ebcba74d821e/download/pre-2012-hospital-annual-financial-disclosure-report-complete-data-set-byspaf.zip"
+n="pre-2012-hospital-annual-financial-disclosure-report-complete-data-set-byspaf.zip"
+if [ ! -f $n ]; then wget $f; fi
+
