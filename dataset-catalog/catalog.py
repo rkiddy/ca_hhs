7426463467d0609ee05c08244b8d5f6acf606914
Ray Kiddy <ray@ganymede.org>
2024-12-31 18:10:21 -0800

    Fetch the catalog data via selenium.

--- dataset-catalog/catalog.py
+++ dataset-catalog/catalog.py
@@ -1,126 +1,31 @@
-import argparse
-import csv
 import sys
-
-import requests
-from sqlalchemy import create_engine
 
 sys.path.append('..')
 
-import config
-
-cfg = config.cfg()
-
-engine = create_engine(f"mysql+pymysql://{cfg['USR']}:{cfg['PWD']}@{cfg['HOST']}/{cfg['DB']}")
-conn = engine.connect()
-
-
-def arguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--check-urls', action='store_true', help="check existence of dataset main page on web")
-
-    return parser.parse_args()
-
-
-def db_exec(eng, this_sql):
-    # print(f"sql: {sql}")
-    if this_sql.strip().startswith('select'):
-        return [dict(row) for row in eng.execute(this_sql).fetchall()]
-    else:
-        # print(f"sql: {this_sql}")
-        return eng.execute(this_sql)
-
-
-def fix_key(start_key):
-     return start_key.lower().replace(' ', '_').replace('/', '_').replace('-', '_')
-
-
-def fix_value(start_value):
-     return start_value.replace("'", "''").replace('%', '%%')
+import csv_helper as csv
 
 
 if __name__ == '__main__':
 
-    args = arguments()
+    tables = {'catalog': 'catalog.csv'}
 
-    if args.check_urls:
-        for row in db_exec(conn, "select dataset_url from catalog"):
-            url = row['dataset_url']
+    types = {'catalog': {'description': 'text',
+                         'program': 'text',
+                         'temporal_coverage': 'text',
+                         'spatial_geographic_coverage': 'text',
+                         'de_identification_method': 'text',
+                         'data_collection_tool': 'text',
+                         'additional_limitations': 'text',
+                         'additional_information': 'text',
+                         'related_resources': 'text',
+                         'secondary_sources': 'text',
+                         'citation': 'text',
+                         'tags': 'text'}}
 
-            status = '000'
+    replaces = {}
+    # {'bad_name1': 'good_name1', 'bad_name2': 'good_name2'}
 
-            try:
-                r = requests.get(url)
-                status = r.status_code
-            except:
-                traceback.print_exc()
-                status='000'
+    csv.create_tables(tables, types, replaces)
+    csv.read_data(tables, types, replaces)
 
-            print(f"response: {status} url: {url}")
-        quit()
-
-    lengths = dict()
-    types = dict()
-
-    row_num = 0
-
-    all_keys = list()
-
-    with open('dataset-catalog.csv', newline='') as csvfile:
-        rdr = csv.DictReader(csvfile)
-        for row in rdr:
-            row_num += 1
-
-            if row_num == 1:
-                for key1 in row.keys():
-                    key2 = fix_key(key1)
-                    all_keys.append(key2)
-                    lengths[key2] = 0
-                    types[key2] = None
-
-            for key1 in row.keys():
-                key2 = fix_key(key1)
-                if len(row[key1]) > lengths[key2]:
-                    lengths[key2] = len(row[key1])
-                if row[key1].isnumeric():
-                    if types[key2] is None:
-                        types[key2] = 'int'
-                else:
-                    types[key2] = 'string'
-
-    # print(f"row_num: {row_num}")
-    # print(f"lengths: {lengths}")
-    # print(f"types: {types}")
-    # print("\n")
-
-    cols = list()
-
-    for key in all_keys:
-        if types[key] == 'int':
-            cols.append(f"{key} integer")
-        else:
-            if lengths[key] < 1000:
-                cols.append(f"{key} varchar({lengths[key]})")
-            else:
-                cols.append(f"{key} text")
-
-    db_exec(conn, "drop table if exists catalog")
-
-    sql = f"create table catalog ({', '.join(cols)});"
-    db_exec(conn, sql)
-
-    with open('dataset-catalog.csv', newline='') as csvfile:
-        rdr = csv.DictReader(csvfile)
-        for row in rdr:
-            sql = f"insert into catalog (_id) values ({row['_id']})"
-            db_exec(conn, sql)
-
-            for key1 in list(row.keys())[1:]:
-                key2 = fix_key(key1)
-                sql = f"update catalog set {key2} = '{fix_value(row[key1])}' where _id = {row['_id']}"
-                # print(f"sql: {sql}")
-                db_exec(conn, sql)
-
-    sql = "insert into updates values ('dataset-catalog', unix_timestamp(now()))"
-    db_exec(conn, sql)
-
+    print("")
