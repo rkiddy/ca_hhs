feef9dbd10aecfe50fb093541f28730425a0a5cc
Ray Kiddy <ray@ganymede.org>
2024-12-31 17:57:38 -0800

    Because of problem column name.

--- california-women-infants-and-children-program-redemption-by-county/redemption.py
+++ california-women-infants-and-children-program-redemption-by-county/redemption.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 sys.path.append('..')
 
@@ -7,20 +8,23 @@ import csv_helper as csv
 
 if __name__ == '__main__':
 
-    tables = {'wic_part_a_redemptions_by_vendor_county':
-                  '2010-2018-part-a-wic-redemptions-by-vendor-county-with-family-counts.csv',
-              'wic_part_b_redemptions_by_vendor_county':
-                  '2019-2020-part-b-wic-redemptions-by-vendor-county-with-family-counts.csv',
-              'wic_part_c_ebt_redemptions_by_vendor_county':
-                  '2021-2022-forward-part-c-wic-ebt-redemptions-by-vendor-county-with-family-counts.csv',
-              'wic_redemptions_by_participant_category':
-                  'wic-redemption-by-vendor-county-by-participant-category-data-2010-2018-.csv'}
+    tables = dict()
+    files = [f for f in os.listdir() if f.endswith('.csv')]
+
+    for f in files:
+        key = f.replace('-', '_')
+        if len(key) > 64:
+            key = key[:63]
+
+        tables[key] = f
 
     types = {}
     # {'table': {'col': 'type'}}
 
-    replaces = {'year_month': 'year_month_str'}
+    replaces = {}
     # {'bad_name1': 'good_name1', 'bad_name2': 'good_name2'}
+
+    csv.fix_file('wic-redemption-by-vendor-county-by-participant-category-data-2010-2018-.csv')
 
     csv.create_tables(tables, types, replaces)
     csv.read_data(tables, types, replaces)
