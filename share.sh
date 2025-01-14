2b20ee8b973f331ec2cef0a557f64a0319f9d392
Ray Kiddy <ray@ganymede.org>
2025-01-14 11:45:56 -0800

    Add doc, add --dry-run param, and is callable with just one dataset.

--- share.sh
+++ share.sh
@@ -8,17 +8,43 @@ if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     exit
 fi
 
-echo "select table_name from csv_sources where share = 1;" | \
-    ssh opencal mysql --skip-column-names ca_hhs_meta | \
-    sed 's/ *//g' | \
-    awk '{print "mysqldump ca_hhs "$0" | ssh opencal mysql ca_hhs"}' > /tmp/share.sql
-
 if [ "$1" = "--dry-run" ]; then
+    dry_run="yes"
+    shift
+else
+    dry_run="no"
+fi
+
+if [ "$1" != "" ]; then
+
+    ( echo "select s1.table_name from datasets d1, csv_sources s1 "
+      echo "where d1.pk = s1.ds_pk and d1.name = '"$1"' and share = 1;" ) | \
+        ssh opencal mysql --skip-column-names ca_hhs_meta > /tmp/sharable_$$.txt
+    if [ ! -s /tmp/sharable_$$.txt ]; then
+       exit
+    else
+       echo "sharing:"
+       cat /tmp/sharable_$$.txt | awk '{print "    "$0}'
+    fi
+
+else
+
+    echo "select table_name from csv_sources where share = 1;" | \
+        ssh opencal mysql --skip-column-names ca_hhs_meta > /tmp/sharable_$$.txt
+fi
+
+cat /tmp/sharable_$$.txt | \
+    sed 's/ *//g' | \
+    awk '{print "mysqldump ca_hhs "$0" | ssh opencal mysql ca_hhs"}' > /tmp/share_$$.sh
+
+if [ $dry_run = "yes" ]; then
     echo ""
-    cat /tmp/share.sql
+    cat /tmp/share_$$.sh
     echo ""
     exit
 else
-    cat /tmp/share.sql | bash -vx
+    HOME=/home/ray bash /tmp/share_$$.sh
 fi
+
+/bin/rm -f /tmp/sharable_$$.txt /tmp/share_$$.sh
 
