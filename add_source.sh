bb1179b812a69cda18e611dadee7fea863275576
Ray Kiddy <ray@ganymede.org>
2025-01-18 12:03:38 -0800

    helpful utility.

--- add_source.sh
+++ add_source.sh
@@ -1,9 +1,11 @@
 
-usage="usage: bash add_source.sh <dataset name> <table name> <file name> [ <sheet name> ]"
+usage="usage: bash add_source.sh <dataset name> ( <table name> | NEW ) <file name> [ <sheet name> ]"
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo ""
     echo $usage
+    echo ""
+    echo "A helpful utility script. If the table name is NEW, then it will be generated fthrom the file."
     echo ""
     exit
 fi
@@ -34,8 +36,18 @@ else
     sn="'"$sn"'"
 fi
 
-( echo "insert into sources (ds_pk, auto_run, share, file_name, sheet_name, table_name)"
-  echo "values"
-  echo "((select pk from datasets where name = '"$ds"'), 1, 0, '"$fn"', "$sn", '"$tn"');" ) | \
-    ssh opencal mysql ca_hhs_meta
+if [ $tn != "NEW" ]; then
+    ( echo "insert into sources (ds_pk, auto_run, share, file_name, sheet_name, table_name)"
+      echo "values"
+      echo "((select pk from datasets where name = '"$ds"'), 1, 0, '"$fn"', "$sn", '"$tn"');" ) | \
+        ssh opencal mysql ca_hhs_meta
+fi
 
+if [ "$tn" = "NEW" ]; then
+    next_tn=`echo $fn | sed 's/-/_/g' | sed 's/.xlsx//'`
+
+    ( echo "insert into sources (ds_pk, auto_run, share, file_name, sheet_name, table_name)"
+      echo "values"
+      echo "((select pk from datasets where name = '"$ds"'), 1, 0, '"$fn"', "$sn", '"$next_tn"');" ) | \
+        ssh opencal mysql ca_hhs_meta
+fi
