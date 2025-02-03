928a4d8cd344c54f4831778ebd816015ec2ed685
Ray Kiddy <ray@ganymede.org>
2025-02-02 16:14:30 -0800

    Fix for finding offsets to coordinate locations.

--- excel_helper.py
+++ excel_helper.py
@@ -52,7 +52,7 @@ def row_values_starting(ws, location, length=None):
             cols.append(cell_value_str(ws, clocation))
 
             ccell = cell_to_right_of(ws, clocation)
-            clocation = f"{ccell.column_letter}{ccell.row}"
+            clocation = ccell.coordinate
             cvalue = cell_value(ws, clocation)
 
     return cols
@@ -105,24 +105,83 @@ def cell_value_str(ws, key):
         return str(ws[key].value)
 
 
+def coordinate_parts(c):
+    """Utility function for separating the two parts of an excel location,
+       such as 'A5', or 'XYZ12'."""
+
+    letters = list()
+    numbers = list()
+    for ltr in list(c):
+        if ltr.isalpha():
+            letters.append(ltr)
+        else:
+            numbers.append(ltr)
+    ltrs = ''.join(letters)
+    num = int(''.join(numbers))
+    return [ltrs, num]
+
+
+def column_offset(col, inc):
+    """Given a column part of a excel cell location, such as 'A' or 'AF', return the one
+       to the right (inc = 1) or the left (inc = -1)."""
+    alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    parts = list(col)
+    if inc == 1:
+        # handle the easy case
+        if parts[-1] in list(alpha)[:-1]:
+            val = ord(parts[-1])
+            nval = chr(val+1)
+            parts[-1] = nval
+            return ''.join(parts)
+        # adding from Z.
+        if parts[-1] == 'Z':
+            parts[-1] = 'A'
+            parts.append('A')
+            return ''.join(parts)
+    if inc == -1:
+        # handle the easy case
+        if parts[-1] in list(alpha)[1:]:
+            val = ord(parts[-1])
+            nval = chr(val-1)
+            parts[-1] = nval
+            return ''.join(parts)
+        if parts[-1] == 'A' and len(parts) > 1:
+            if parts[-2] == 'A':
+                parts.pop()
+                parts[-1] = 'Z'
+            else:
+                parts.pop()
+                val = ord(parts[-1])
+                nval = chr(val-1)
+                parts[-1] = nval
+                parts.append('Z')
+            return ''.join(parts)
+
+    raise Exception(f"invalid column name passed: {col}, inc: {col}")
+
+
 def cell_to_right_of(ws, key):
-    c = ws[key]
-    return c.offset(0,1)
+    parts = coordinate_parts(key)
+    ncol = column_offset(parts[0], 1)
+    return ws[f"{ncol}{parts[1]}"]
 
 
 def cell_to_left_of(ws, key):
-    c = ws[key]
-    return c.offset(0,-1)
+    parts = coordinate_parts(key)
+    ncol = column_offset(parts[0], -1)
+    return ws[f"{ncol}{parts[1]}"]
 
 
 def cell_above(ws, key):
-    c = ws[key]
-    return c.offset(-1,0)
+    parts = coordinate_parts(key)
+    nrow = parts[1] - 1
+    return ws[f"{parts[0]}{nrow}"]
 
 
 def cell_below(ws, key):
-    c = ws[key]
-    return c.offset(1,0)
+    parts = coordinate_parts(key)
+    nrow = parts[1] + 1
+    return ws[f"{parts[0]}{nrow}"]
 
 
 def all_empty(values):
@@ -131,12 +190,12 @@ def all_empty(values):
 
 
 def location_of(c):
-    return f"{c.column_letter}{c.row}"
+    return c.coordinate
 
 
 # TODO the create_tables and read_tables can be refactored to find once and produce different statements.
 #
-def create_tables(tables, types={}, replaces={}, length_pad=0, verbose=False):
+def create_tables(tables, types={}, replaces={}, length_pad=0, verbose=False, topmost_data=2, leftmost_data='A'):
     """Do the right thing for most csv files based on the column header strings found.
        Also determines the optimal length for a string column, though this process reads
        all of the data and so is probably not optimal and is probably doable in a smarter
@@ -162,13 +221,15 @@ def create_tables(tables, types={}, replaces={}, length_pad=0, verbose=False):
 
             ws = wb.active
 
+            first_col_head = f"{leftmost_data}1"
+
             # Find the column tops.
             #
             # TODO: Finding column tops may not be as easy as going to location A1.
             #
             # Turn the list of columns into a dictionary with column names going to 0.
             #
-            cols = sql_helper.fix_col_heads(row_values_starting(ws, 'A1'))
+            cols = sql_helper.fix_col_heads(row_values_starting(ws, first_col_head))
             cols = dict(zip(cols, [0] * len(cols)))
 
             if verbose:
@@ -176,13 +237,13 @@ def create_tables(tables, types={}, replaces={}, length_pad=0, verbose=False):
 
             done = False
 
-            idx = 2
+            idx = topmost_data
 
             # Read through content, row at a time, noting max lengths of string values.
             #
             while not done:
 
-                current = f"A{idx}"
+                current = f"{leftmost_data}{idx}"
                 values = row_values_starting(ws, current, length=len(cols))
 
                 if verbose:
@@ -223,7 +284,7 @@ def create_tables(tables, types={}, replaces={}, length_pad=0, verbose=False):
             traceback.print_exc()
 
 
-def read_data(tables, types={}, replaces={}, start_row=None, bucket=1000, verbose=False):
+def read_data(tables, types={}, replaces={}, start_row=None, bucket=1000, verbose=False, topmost_data=2, leftmost_data='A'):
     """Reads CSV data and puts it into a table."""
 
     if verbose:
@@ -248,9 +309,11 @@ def read_data(tables, types={}, replaces={}, start_row=None, bucket=1000, verbos
                         wb.active = wb.get_sheet_by_name(sheet)
             ws = wb.active
 
+            first_col_head = f"{leftmost_data}1"
+
             # Find the column tops.
             #
-            cols = sql_helper.fix_col_heads(row_values_starting(ws, 'A1'))
+            cols = sql_helper.fix_col_heads(row_values_starting(ws, first_col_head))
             cols = dict(zip(cols, [0] * len(cols)))
 
             prefix = f"insert into {table} ({', '.join(cols)}) values"
@@ -262,7 +325,7 @@ def read_data(tables, types={}, replaces={}, start_row=None, bucket=1000, verbos
 
             done = False
 
-            idx = 2
+            idx = topmost_data
 
             added = 0
 
@@ -270,7 +333,7 @@ def read_data(tables, types={}, replaces={}, start_row=None, bucket=1000, verbos
             #
             while not done:
 
-                current = f"A{idx}"
+                current = f"{leftmost_data}{idx}"
                 values = row_values_starting(ws, current, length=len(cols))
 
                 if verbose:
