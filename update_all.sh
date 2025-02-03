0abd3ba1878ca108f183f2e9834a1afd191f0814
Ray Kiddy <ray@ganymede.org>
2025-02-03 09:35:51 -0800

    no longer needed.

--- update_all.sh
+++
@@ -1,8 +0,0 @@
-
-find *-* -name update.sh | \
-    sed 's/\/update.sh//' | \
-    awk '{print "( cd "$0"; bash ./update_generic.sh '$*' )"}' | \
-    /bin/bash
-
-mysqldump --no-data ca_hhs | grep -v '^/' > tables.sql
-
