af92fbbeca69b118fc30ee760a22d34c3ee02acf
Ray Kiddy <ray@ganymede.org>
2025-04-11 14:51:05 -0700

    late changes

--- updatables.py
+++ updatables.py
@@ -11,17 +11,36 @@ import utils
 def arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--exec', '-x', action='store_true', help="Locate datasets to update.")
-    parser.add_argument('--file-filter', help="Update only datasets with files that have this extension.")
+    parser.add_argument('--filter', help="Update only datasets with files that have this extension.")
     parser.add_argument('--all', action='store_true', help="Display all datasets, regardless of update status.")
+    parser.add_argument('--datasets', help="File with list of datasets to update.")
     parser.add_argument('--verbose', '-v', action='store_true')
+    parser.add_argument('--brief', action='store_true', help="Display only dataset names and nothing else.")
     return parser.parse_args()
+
+
+def load_datasets_file(name):
+    if name is None:
+        return []
+    dss = list()
+    with open(name, 'r') as f:
+        for line in f.readlines():
+            dss.append(line.strip())
+    f.close()
+    return dss
 
 
 if __name__ == '__main__':
 
     args = arguments()
 
-    ds = utils.hcai_data()
+    overrides = load_datasets_file(args.datasets)
+
+    try:
+        ds = utils.hcai_data()
+    expect:
+        print("There was an error fetching from https://opencalaccess.org/hcaijson")
+        ds = {}
 
     for name in ds:
 
@@ -40,11 +59,11 @@ if __name__ == '__main__':
         else:
             exts = ''
 
-        if not ca_update or not my_update or my_update < ca_update or args.all:
+        if not ca_update or not my_update or my_update < ca_update or args.all or name in overrides:
             if args.exec:
 
-                if args.file_filter:
-                    if 'ext' not in ds[name] or args.file_filter not in ds[name]['ext']:
+                if args.filter:
+                    if 'ext' not in ds[name] or args.filter not in ds[name]['ext']:
                         continue
 
                 if os.path.isfile(f"{name}/deets.sh"):
@@ -56,9 +75,12 @@ if __name__ == '__main__':
 
             else:
 
-                if args.file_filter:
-                    if 'ext' not in ds[name] or args.file_filter not in ds[name]['ext']:
+                if args.filter:
+                    if 'ext' not in ds[name] or args.filter not in ds[name]['ext']:
                         continue
 
-                print(f"ds: {name} my: {my_update} ca: {ca_update} exts: {exts}")
+                if args.brief:
+                    print(f"ds: {name}")
+                else:
+                    print(f"ds: {name} my: {my_update} ca: {ca_update} exts: {exts}")
 
